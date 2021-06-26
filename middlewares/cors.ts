943d865c993541aca2f5a96a672eeb835fd0035c
Batiste Bieler <batiste.bieler@gmail.com>
2021-06-26 14:47:36 +0200

    Add cors middleware

---
+++ middlewares/cors.ts
@@ -0,0 +1,11 @@
+
+import * as http from 'http';
+import { ResponseMiddleware } from '../schema';
+
+export function createCorsMiddleware(domain: string): ResponseMiddleware {
+  return function corsMiddleware(upstreamResponse: http.IncomingMessage) {
+    const headers = upstreamResponse.headers
+    headers['access-control-allow-credentials'] = 'true'
+    headers['access-control-allow-origin'] = domain
+  }
+}
