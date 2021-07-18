c0403137f843357bb3efecfa283b7228add2d363
Batiste Bieler <batiste.bieler@gmail.com>
2021-07-18 18:06:48 +0200

    Small cleanup

--- middlewares/rateLimit.ts
+++ middlewares/rateLimit.ts
@@ -21,8 +21,9 @@ interface KeyLimit {
 
 export function createRateLimitationMiddleware(options: RateLimitsOptions): RequestMiddleware {
   const url = options.redisURL || process.env.REDIS_URL || 'redis://localhost:6379'
+  const timeWindow = options.timeWindow || 60
   const store = new RedisStore({
-    expiry: options.timeWindow || 60,
+    expiry: timeWindow,
     redisURL: url
   });
   logger.log(`Rate limitation middleware connected on ${url}`)
@@ -30,32 +31,32 @@ export function createRateLimitationMiddleware(options: RateLimitsOptions): Requ
   return async function rateLimitMiddleware(clientRequest: Request, clientResponse: http.ServerResponse) {
 
     const keysLimits = options.keysLimits(clientRequest)
-    if (keysLimits) {
-      let resolved = false;
-      const checkLimits = new Promise((resolve) => {
-        for(let i=0; i < keysLimits.length; i++) {
-          const limit = keysLimits[i]
-          store.incr(limit.key, (err: any, v: number, ttl: number) => {
-            if (resolved) { return }
-            resolved = true
-            if (options.setHeaders) {
-              clientResponse.setHeader(`RateLimit-Limit-${limit.name || i}`, v)
-              clientResponse.setHeader(`RateLimit-Remaining-${limit.name || i}`, limit.limit - v)
-            }
-            if(v <= limit.limit) {
-              resolve(false)
-            } else {
-              resolve(v)
-            }
-          })
-        }
-      })
-      const limitReached = await checkLimits
-      if(limitReached !== false) {
-        clientResponse.writeHead(429)
-        clientResponse.end('Rate limit reached')
-        return true
+
+    let resolved = false;
+    const checkLimits = new Promise((resolve) => {
+      for(let i=0; i < keysLimits.length; i++) {
+        const limit = keysLimits[i]
+        store.incr(limit.key, (err: any, v: number, ttl: number) => {
+          if (resolved) { return }
+          resolved = true
+          if (options.setHeaders) {
+            clientResponse.setHeader(`RateLimit-Limit-${limit.name || i}`, v)
+            clientResponse.setHeader(`RateLimit-Remaining-${limit.name || i}`, limit.limit - v)
+          }
+          if(v <= limit.limit) {
+            resolve(false)
+          } else {
+            resolve(v)
+          }
+        })
       }
+    })
+
+    const limitReached = await checkLimits
+    if(limitReached !== false) {
+      clientResponse.writeHead(429)
+      clientResponse.end(`Rate limit reached`)
+      return true
     }
 
     return false
