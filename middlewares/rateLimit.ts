6e04c358f41fb6a696b407d4a7df2126ea99e76a
Batiste Bieler <batiste.bieler@pricehubble.com>
2021-10-17 10:15:48 +0100

    Improve documentation, fix rate limitation behaviour

--- middlewares/rateLimit.ts
+++ middlewares/rateLimit.ts
@@ -5,18 +5,28 @@ import * as redis from "redis";
 import { logger, onShutdown } from "../logs";
 import { Request } from "../schema";
 
+  /** A list of rate limitation to apply on a single time window */
 export interface RateLimitsOptions {
+  /** Function returning a list of rate limit to apply */
   keysLimits: (clientRequest: Request) => KeyLimit[];
+  /* Redis URL to connect to. Default to provess.env.REDIS_URL or redis://localhost:6379*/
   redisURL?: string;
-  /** Time window duration in seconds for this rate limitation */
+  /** Time window duration in seconds for this rate limitation. Default is 1 minute. */
   timeWindow?: number;
-  /** Set the rate limits response headers if true */
+  /** Set the rate limits response headers if true. E.g. RateLimit-Limit-{key.name} */
   setHeaders?: boolean;
 }
 
+/** Key used to store the request counter in Redis with the limit */
 export interface KeyLimit {
+  /** Key used to store the request counter. E.g. "global", "user-${user.id}" */
   key: string;
+  /** Maximum limit of requests that are allowed on this key */
   limit: number;
+  /** Name used in the response header for this limit. e.g.
+   * RateLimit-Limit-Global
+   * RateLimit-Limit-User
+   */
   name?: string;
 }
 
@@ -48,15 +58,11 @@ export function createRateLimitationMiddleware(
   ) {
     const keysLimits = options.keysLimits(clientRequest);
 
-    let resolved = false;
-    const checkLimits = new Promise((resolve) => {
-      for (let i = 0; i < keysLimits.length; i++) {
+    let anyLimitReached = false;
+    for (let i = 0; i < keysLimits.length; i++) {
+      const limitReached = await new Promise((resolve) => {
         const limit = keysLimits[i];
         store.incr(limit.key, (err: any, v: number, ttl: number) => {
-          if (resolved) {
-            return;
-          }
-          resolved = true;
           if (options.setHeaders) {
             clientResponse.setHeader(`RateLimit-Limit-${limit.name || i}`, v);
             clientResponse.setHeader(
@@ -64,17 +70,19 @@ export function createRateLimitationMiddleware(
               limit.limit - v
             );
           }
-          if (v <= limit.limit) {
-            resolve(false);
+          if (v > limit.limit) {
+            resolve(true);
           } else {
-            resolve(v);
+            resolve(false);
           }
         });
+      });
+      if (limitReached) {
+        anyLimitReached = true
       }
-    });
+    }
 
-    const limitReached = await checkLimits;
-    if (limitReached !== false) {
+    if (anyLimitReached) {
       clientResponse.writeHead(429);
       clientResponse.end(`Rate limit reached`);
       return true;
