d16f2072901a571b00b346227beec3c80feaad3e
Batiste Bieler <batiste.bieler@pricehubble.com>
2021-11-20 11:42:40 +0000

    Linting

--- tests/gateway.spec.ts
+++ tests/gateway.spec.ts
@@ -1,25 +1,25 @@
 import { Config, IMatcher } from "../schema";
 import supertest from "supertest";
 import { createGateway } from "../gateway";
-import * as http from "http"
+import * as http from "http";
 import { createJWTVerificationMiddleware } from "../middlewares/JWTVerification";
 
-const BACKEND1_PORT = 8001
-const BACKEND2_PORT = 8002
+const BACKEND1_PORT = 8001;
+const BACKEND2_PORT = 8002;
 
 export function getConfig(): Config {
   const matchers: IMatcher[] = [
     // BACKEND1_PORT
     {
-      uris: ['/admin/'],
-      upstream: 'localhost',
+      uris: ["/admin/"],
+      upstream: "localhost",
       port: BACKEND1_PORT,
       stripeUri: true,
     },
     // BACKEND2_PORT
     {
-      uris: ['/admin/blop', '/test'],
-      upstream: 'localhost',
+      uris: ["/admin/blop", "/test"],
+      upstream: "localhost",
       port: BACKEND2_PORT,
     },
     // JWT
@@ -31,26 +31,26 @@ export function getConfig(): Config {
         createJWTVerificationMiddleware(["old-secret", "secret"]),
       ],
     },
-  ]
-  return { matchers }
+  ];
+  return { matchers };
 }
 
 const requestListener1: http.RequestListener = function (req, res) {
   res.setHeader("Content-Type", "application/json");
   res.writeHead(200);
-  res.end(JSON.stringify({"type": "backend1", url: req.url}));
+  res.end(JSON.stringify({ type: "backend1", url: req.url }));
 };
 
 const requestListener2: http.RequestListener = function (req, res) {
   res.setHeader("Content-Type", "application/json");
   res.writeHead(200);
-  res.end(JSON.stringify({"type": "backend2", url: req.url}));
+  res.end(JSON.stringify({ type: "backend2", url: req.url }));
 };
 
-describe('gateway', () => {
-  let backend1: http.Server
-  let backend2: http.Server
-  let gateway: http.Server
+describe("gateway", () => {
+  let backend1: http.Server;
+  let backend2: http.Server;
+  let gateway: http.Server;
 
   beforeAll(() => {
     gateway = createGateway(getConfig(), 3000);
@@ -59,43 +59,47 @@ describe('gateway', () => {
   });
 
   afterAll(() => {
-    backend1.close()
-    backend2.close()
-    gateway.close()
+    backend1.close();
+    backend2.close();
+    gateway.close();
   });
 
-  it('404', async () => {
-    const request = supertest(gateway)
-    let response = await request.get('/does-not-exist/')
+  it("404", async () => {
+    const request = supertest(gateway);
+    let response = await request.get("/does-not-exist/");
 
-    expect(response.status).toEqual(404)
+    expect(response.status).toEqual(404);
   });
 
-  it('matchUris', async () => {
-    const request = supertest(gateway)
-    let response = await request.get('/admin/')
-        
-    expect(response.status).toEqual(200)
-    expect(JSON.parse(response.text).type).toEqual("backend1")
-    expect(JSON.parse(response.text).url).toEqual("/")
+  it("matchUris", async () => {
+    const request = supertest(gateway);
+    let response = await request.get("/admin/");
 
-    response = await request.get('/admin/extra/url')
-    expect(JSON.parse(response.text).url).toEqual("/extra/url")
+    expect(response.status).toEqual(200);
+    expect(JSON.parse(response.text).type).toEqual("backend1");
+    expect(JSON.parse(response.text).url).toEqual("/");
 
-    response = await request.get('/test/')
-    expect(response.status).toEqual(200)
-    expect(JSON.parse(response.text).type).toEqual("backend2")
+    response = await request.get("/admin/extra/url");
+    expect(JSON.parse(response.text).url).toEqual("/extra/url");
+
+    response = await request.get("/test/");
+    expect(response.status).toEqual(200);
+    expect(JSON.parse(response.text).type).toEqual("backend2");
   });
 
-  it('JWT', async () => {
-    let request = supertest(gateway)
-    let response = await request.get('/jwt/')
-        
-    expect(response.status).toEqual(401)
+  it("JWT", async () => {
+    let request = supertest(gateway);
+    let response = await request.get("/jwt/");
 
-    request = supertest(gateway)
-    response = await request.get('/jwt/').set('Authorization', 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZXN0IjoiMSJ9.JDfCu69YMtGgGfImZq9j0xapAIkEM9Hf6VM_wvd4Z9M')
-    expect(response.status).toEqual(200)
+    expect(response.status).toEqual(401);
+
+    request = supertest(gateway);
+    response = await request
+      .get("/jwt/")
+      .set(
+        "Authorization",
+        "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZXN0IjoiMSJ9.JDfCu69YMtGgGfImZq9j0xapAIkEM9Hf6VM_wvd4Z9M"
+      );
+    expect(response.status).toEqual(200);
   });
-
 });
--- tests/matcher.spec.ts
+++ tests/matcher.spec.ts
@@ -2,51 +2,50 @@ import { matchHosts, matchUris, matchRequest } from "../match";
 import { IMatcher, Request } from "../schema";
 import * as http from "http";
 
-
-describe('matchUris', () => {
-
-  it('matchUris', async () => {
-    expect(matchUris(['/admin/'], '/admin')).toEqual(false)
-    expect(matchUris(['/admin'], '/admin')).toEqual('/admin')
-    expect(matchUris(['/admin'], '/admin/hello')).toEqual('/admin')
-    expect(matchUris([/^(\/admin)\/.*/], '/admin/hello')).toEqual('/admin')
+describe("matchUris", () => {
+  it("matchUris", async () => {
+    expect(matchUris(["/admin/"], "/admin")).toEqual(false);
+    expect(matchUris(["/admin"], "/admin")).toEqual("/admin");
+    expect(matchUris(["/admin"], "/admin/hello")).toEqual("/admin");
+    expect(matchUris([/^(\/admin)\/.*/], "/admin/hello")).toEqual("/admin");
   });
-
 });
 
-describe('matchHosts', () => {
-
-  it('matchHosts', async () => {
-    expect(matchHosts(['example.com', 'test.com'], 'test.com')).toEqual('test.com')
-    expect(matchHosts(['example.com', 'test.com'], 'test1.com')).toEqual(false)
-    expect(matchHosts(['example.com', /^(.*)\.com/], 'test1.com')).toEqual('test1.com')
+describe("matchHosts", () => {
+  it("matchHosts", async () => {
+    expect(matchHosts(["example.com", "test.com"], "test.com")).toEqual(
+      "test.com"
+    );
+    expect(matchHosts(["example.com", "test.com"], "test1.com")).toEqual(false);
+    expect(matchHosts(["example.com", /^(.*)\.com/], "test1.com")).toEqual(
+      "test1.com"
+    );
   });
-
 });
 
-describe('matchRequest', () => {
-
-  it('matchRequest', async () => {
+describe("matchRequest", () => {
+  it("matchRequest", async () => {
     const matcher: IMatcher[] = [
-      {'upstream': 'example.com', 'uris': ['/hello']},
-      {'upstream': 'example.com', 'uris': ['/admin']},
-      {'upstream': 'test.com', 'hosts': ['example.com']},
-      {'upstream': 'test.com'}
-    ]
-    const socket: any = {}
-    const request: Request = new http.IncomingMessage(socket) as any
-    request.url = '/admin'
-    request.headers['host'] = 'example.com'
+      { upstream: "example.com", uris: ["/hello"] },
+      { upstream: "example.com", uris: ["/admin"] },
+      { upstream: "test.com", hosts: ["example.com"] },
+      { upstream: "test.com" },
+    ];
+    const socket: any = {};
+    const request: Request = new http.IncomingMessage(socket) as any;
+    request.url = "/admin";
+    request.headers["host"] = "example.com";
 
-    console.log(matchRequest(matcher, request))
-    expect(matchRequest(matcher, request)?.criteria).toEqual({ uri: '/admin' })
-    request.url = '/'
-    expect(matchRequest(matcher, request)?.criteria).toEqual({ host: 'example.com' })
-    request.url = '/hello/blop'
-    expect(matchRequest(matcher, request)?.criteria).toEqual({ uri: '/hello' })
-    request.url = '/nop'
-    request.headers['host'] = 'nop.com'
-    expect(matchRequest(matcher, request)?.criteria).toEqual({})
+    console.log(matchRequest(matcher, request));
+    expect(matchRequest(matcher, request)?.criteria).toEqual({ uri: "/admin" });
+    request.url = "/";
+    expect(matchRequest(matcher, request)?.criteria).toEqual({
+      host: "example.com",
+    });
+    request.url = "/hello/blop";
+    expect(matchRequest(matcher, request)?.criteria).toEqual({ uri: "/hello" });
+    request.url = "/nop";
+    request.headers["host"] = "nop.com";
+    expect(matchRequest(matcher, request)?.criteria).toEqual({});
   });
-
 });
